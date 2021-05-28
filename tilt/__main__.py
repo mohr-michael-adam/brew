d0f1116fb68cf6e25e7d7a12b5497a1e7f604535
mohr-michael-adam <mohr_michael_a@yahoo.ca>
2021-05-28 13:24:49 -0400

    Configure service to run the beacon reader

--- tilt/__main__.py
+++ tilt/__main__.py
@@ -6,31 +6,35 @@ DEFAULT_LOG_FILE = 'tilt.log'
 DEFAULT_LOG_LEVEL = 'WARNING'
 
 
-def _get_config():
+def _get_logger(args):
+    log_file = DEFAULT_LOG_FILE
+    log_level = DEFAULT_LOG_LEVEL
+
+    if args.log_file:
+        log_file = args.log_file
+
+    if args.log_level:
+        log_level = args.log_level
+
+    logging.basicConfig(filename=log_file, level=log_level, format='%(asctime)s - %(levelname)s - %(message)s',
+                        datefmt='%d-%b-%y %H:%M:%S')
+
+    return logging.getLogger()
+
+
+def _get_args():
 
     parser = argparse.ArgumentParser(description='Read measurements from the Tilt hydrometer and push to a URL.')
     parser.add_argument('--log-file', dest='log_file', help='The log file')
     parser.add_argument('--log-level', dest='log_level', help='The log level')
 
-    parsed_args = parser.parse_args()
-
-    log_file = DEFAULT_LOG_FILE
-    log_level = DEFAULT_LOG_LEVEL
-
-    if parsed_args.log_file:
-        log_file = parsed_args.log_file
-
-    if parsed_args.log_level:
-        log_level = parsed_args.log_level
-
-    logging.basicConfig(filename=log_file, level=log_level, format='%(asctime)s - %(levelname)s - %(message)s',
-                        datefmt='%d-%b-%y %H:%M:%S')
-    return logging.getLogger()
+    return parser.parse_args()
 
 
 if __name__ == '__main__':
-    logger = _get_config()
+    args = _get_args()
+    logger = _get_logger(args)
 
-    logger.debug("Not much here!")
+    logger.warning("Not much here!")
 
     start_scanner()
