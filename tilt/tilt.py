07bc6892a9a458535fb600d3c523f3532770f42c
mohr-michael-adam <mohr_michael_a@yahoo.ca>
2021-06-04 11:39:12 -0400

    filter out invalid values - format to C and specific gravity

--- tilt/tilt.py
+++ tilt/tilt.py
@@ -5,6 +5,10 @@ from beacontools import BeaconScanner, IBeaconAdvertisement
 
 BLACK_TILT = "a495bb30-c5b1-4b44-b512-1370f02d74de"
 TIME_BETWEEN_PACKETS = 5 * 60
+MIN_TEMP = 0
+MAX_TEMP = 185
+MIN_GRAVITY = 990
+MAX_GRAVITY = 1120
 
 tilt_queue = queue.Queue()
 logger = logging.getLogger()
@@ -20,7 +24,12 @@ def start_scanner():
     while True:
         data = tilt_queue.get()
 
-        logger.info("Timestamp %s, major %d, minor %d" % (data['time'].strftime("%m/%d/%Y, %H:%M:%S:%f"), data['major'], data['minor']))
+        logger.info("Timestamp %s, temp F %d, temp C %1.2f, gravity %1.4f" %
+                    (data['time'].strftime("%m/%d/%Y, %H:%M:%S:%f"),
+                     data['temp'],
+                     round((data['temp'] - 32) * 5.0/9.0),
+                     data['gravity'] * .001)
+                    )
 
 
 def _beacon_callback(bt_addr, rssi, packet, additional_info):
@@ -28,17 +37,29 @@ def _beacon_callback(bt_addr, rssi, packet, additional_info):
     major = packet.major
     minor = packet.minor
 
+    # Data format documented here - https://kvurd.com/blog/tilt-hydrometer-ibeacon-data-format/
+
     data = dict()
 
     data['time'] = datetime.utcnow()
-    data['major'] = major
-    data['minor'] = minor
+    data['temp'] = major
+    data['gravity'] = minor
 
     global last_packet_sent
 
     delta = (data['time'] - last_packet_sent).total_seconds()
 
-    logger.info("UUID %s, major %d, minor %d, time since last packet %d" % (uuid, major, minor, delta))
+    logger.debug("UUID %s, major %d, minor %d, time since last packet %d" % (uuid, major, minor, delta))
+
+    # Valid values documented here - https://tilthydrometer.com/pages/faqs
+
+    if data['temp'] < MIN_TEMP or data['temp'] > MAX_TEMP:
+        logger.debug('Invalid temp %d' % data['temp'])
+        return
+
+    if data['gravity'] < MIN_GRAVITY or data['gravity'] > MAX_GRAVITY:
+        logger.debug('Invalid gravity %d' % data['gravity'])
+        return
 
     if delta > TIME_BETWEEN_PACKETS and packet.uuid == BLACK_TILT:
         tilt_queue.put_nowait(data)
