28681f6c3d274d5de33e9b20750fb7c07a5df6a1
mohr-michael-adam <mohr_michael_a@yahoo.ca>
2021-06-18 20:19:26 -0400

    lambda function will validate signature

--- processor/receiver/receiver.py
+++ processor/receiver/receiver.py
@@ -1,11 +1,41 @@
 import logging
+import json
+
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_v1_5
+from Crypto.Hash import SHA256
+from base64 import b64decode
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
+public_key_path = "sign_pub.key"
+
+
+def verify_sign(public_key, signature, data):
+    pub_key = open(public_key, "r").read()
+    rsakey = RSA.importKey(pub_key)
+    signer = PKCS1_v1_5.new(rsakey)
+    digest = SHA256.new()
+    digest.update(data)
+    if signer.verify(digest, b64decode(signature)):
+        return True
+    return False
+
 
 def lambda_handler(event, context):
     logger.info('Event: %s', event)
+
+    signature = event['signature']
+
+    del event['signature']
+
+    encoded_data = json.dumps(event).encode('utf-8')
+
+    if verify_sign(public_key_path, signature.encode('utf-8'), encoded_data):
+        logger.info('Validated the signature')
+    else:
+        logger.warning('Unable to validate the signature')
 
     response = {}
     return response
--- terraform/terraform.auto.tfvars
+++ terraform/terraform.auto.tfvars
@@ -1,2 +1,2 @@
 # TODO - read from VERSION file
-receiver_version = "0.0.1"
+receiver_version = "0.0.2"
