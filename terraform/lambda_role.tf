7f08b719c5f3732d0d5dae8e367785016030d9b2
mohr-michael-adam <mohr_michael_a@yahoo.ca>
2021-06-28 11:54:22 -0400

    policy attached to our lambda role to interact with dynamo

--- terraform/lambda_role.tf
+++ terraform/lambda_role.tf
@@ -20,3 +20,33 @@ resource "aws_iam_role_policy_attachment" "lambda_ex_attach" {
   role       = aws_iam_role.lambda_ex.name
   policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
 }
+
+resource "aws_iam_policy" "lambda_dynamo" {
+  name        = "lambda_dynamo"
+  path        = "/"
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "ReadWriteTable",
+            "Effect": "Allow",
+            "Action": [
+                "dynamodb:BatchGetItem",
+                "dynamodb:GetItem",
+                "dynamodb:Query",
+                "dynamodb:Scan",
+                "dynamodb:BatchWriteItem",
+                "dynamodb:PutItem",
+                "dynamodb:UpdateItem"
+            ],
+            "Resource": "arn:aws:dynamodb:us-east-1:351511140876:table/brew"
+        }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_dynamo_attach" {
+  role       = aws_iam_role.lambda_ex.name
+  policy_arn = aws_iam_policy.lambda_dynamo.arn
+}
