16c48fe42f6f108e7278e94cf02be5832bd21a3a
mohr-michael-adam <mohr_michael_a@yahoo.ca>
2021-10-06 14:46:41 -0400

    Add policy to allow getObject

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -2,3 +2,24 @@ resource "aws_s3_bucket" "brew_data_bucket" {
   bucket = "mohr-brew-data-bucket"
   acl    = "public-read"
 }
+
+resource "aws_s3_bucket_policy" "brew_data_bucket_public_read_policy" {
+  bucket = aws_s3_bucket.brew_data_bucket.id
+
+  policy = jsonencode({
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "PublicReadGetObject",
+            "Effect": "Allow",
+            "Principal": "*",
+            "Action": [
+                "s3:GetObject"
+            ],
+            "Resource": [
+                "arn:aws:s3:::mohr-brew-data-bucket/*"
+            ]
+        }
+    ]
+  })
+}
