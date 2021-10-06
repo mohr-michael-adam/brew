e5546b11a420538b8672b69a059bdc5343b14ee7
mohr-michael-adam <mohr_michael_a@yahoo.ca>
2021-10-06 14:32:04 -0400

    Setup event bridge to call export lambda

---
+++ terraform/event_bridge.tf
@@ -0,0 +1,19 @@
+resource "aws_cloudwatch_event_rule" "export_brew_event_rule" {
+  name        = "export-brew-event-rule"
+
+  schedule_expression = "cron(0/15 * * * ? *)"
+
+  is_enabled = false
+}
+
+resource "aws_cloudwatch_event_target" "export_brew_event_target" {
+  arn  = aws_lambda_function.exporter_lambda.arn
+  rule = aws_cloudwatch_event_rule.export_brew_event_rule.id
+
+  input = <<EOF
+{
+  "brew": "German Pilsner"
+}
+EOF
+
+}
