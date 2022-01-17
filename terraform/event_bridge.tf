5772d962891e26dd6d85754a23a7ee4d56a1ea7b
mohr-michael-adam <mohr_michael_a@yahoo.ca>
2022-01-17 13:36:19 -0500

    Track Whole Hop Experiment

--- terraform/event_bridge.tf
+++ terraform/event_bridge.tf
@@ -3,7 +3,7 @@ resource "aws_cloudwatch_event_rule" "export_brew_event_rule" {
 
   schedule_expression = "cron(0/15 * * * ? *)"
 
-  is_enabled = false
+  is_enabled = true
 }
 
 resource "aws_cloudwatch_event_target" "export_brew_event_target" {
@@ -12,7 +12,7 @@ resource "aws_cloudwatch_event_target" "export_brew_event_target" {
 
   input = <<EOF
 {
-  "brew": "Dusseldorf Altbier"
+  "brew": "Whole Hop Experiment"
 }
 EOF
 
