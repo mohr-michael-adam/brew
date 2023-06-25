8838e797447012d00e989f752e5db2aa89177721
Michael Mohr <mohr_michael_a@yahoo.ca>
2023-06-25 16:35:52 -0400

    typo

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -133,7 +133,7 @@ resource "aws_s3_bucket_object" "festa_brew_cream_ale_fall_2022_html_object" {
 
 resource "aws_s3_bucket_object" "festa_brew_page_ale_summer_2023_html_object" {
   bucket = aws_s3_bucket.brew_data_bucket.id
-  key    = "Festa Brew Page Ale Summer 2023.html"
+  key    = "Festa Brew Pale Ale Summer 2023.html"
   source = "ui/Festa Brew Pale Ale Summer 2023.html"
   content_type = "text/html"
 }
