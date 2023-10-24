1a45c7a8d61867c52751c10cf723ba893627dbed
Michael Mohr <mohr_michael_a@yahoo.ca>
2023-10-23 21:40:41 -0400

    fix key name

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -140,7 +140,7 @@ resource "aws_s3_bucket_object" "festa_brew_page_ale_summer_2023_html_object" {
 
 resource "aws_s3_bucket_object" "festa_brew_blonde_lager_fall_2023_html_object" {
   bucket = aws_s3_bucket.brew_data_bucket.id
-  key    = "Festa Brew Blonde Lager 2023.html"
+  key    = "Festa Brew Blonde Lager Fall 2023.html"
   source = "ui/Festa Brew Blonde Lager Fall 2023.html"
   content_type = "text/html"
 }
